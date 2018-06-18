4b594ebd6df3818a91c5f0600b4c113794e7f632
Shakira <shakira.git@gmail.com>
2018-06-18 16:04:54 -0700

    adding test

--- main.go
+++ main.go
@@ -7,18 +7,17 @@ import (
 	"io/ioutil"
 )
 
-func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
+func JsonUploader(w http.ResponseWriter, r *http.Request) {
         if r.Body == nil {
                 http.Error(w, "Please send a request body", 400)
                 return
         }
 	bodyBytes, _ := ioutil.ReadAll(r.Body)
 	fmt.Fprintln(w, string(bodyBytes))
-	fmt.Fprintln(w, "Hello world :)")
 }
 
 func main() {
-	http.HandleFunc("/", HomeEndpoint)
+	http.HandleFunc("/upload", JsonUploader)
 	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal(err)
 	}
